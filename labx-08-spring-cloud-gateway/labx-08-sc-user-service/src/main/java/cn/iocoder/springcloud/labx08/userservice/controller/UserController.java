b480a10f98d7a19718787adc89915bb5d8fbcbfe
zuohf <slien1006@163.com>
2020-08-06 19:09:53 +0800

    commit yaml

--- labx-08-spring-cloud-gateway/labx-08-sc-user-service/src/main/java/cn/iocoder/springcloud/labx08/userservice/controller/UserController.java
+++ labx-08-spring-cloud-gateway/labx-08-sc-user-service/src/main/java/cn/iocoder/springcloud/labx08/userservice/controller/UserController.java
@@ -2,14 +2,20 @@ package cn.iocoder.springcloud.labx08.userservice.controller;
 
 import cn.iocoder.springcloud.labx08.userservice.dto.UserAddDTO;
 import cn.iocoder.springcloud.labx08.userservice.dto.UserDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
 
     @GetMapping("/get")
     public UserDTO get(@RequestParam("id") Integer id) {
+        log.info("访问请求get:{}",id);
         return new UserDTO().setId(id)
                 .setName("没有昵称：" + id)
                 .setGender(id % 2 + 1); // 1 - 男；2 - 女
